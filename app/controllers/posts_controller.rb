11dad8f45a5a6e28c64dc9f9dec32266c6880cf1
Amkam <amaduskamara@gmail.com>
2022-03-31 14:44:58 +0000

    Add create post form with simple_form and styled with bulma

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,22 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
